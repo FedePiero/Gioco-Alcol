e1d5d5dfba533c57103500a4203342b5b2885ac1
Federico <federicoluca99@gmail.com>
2023-04-11 22:30:10 +0200

    Fix: restyle Home Page

--- experince-game/src/views/Home.vue
+++ experince-game/src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <el-row :gutter="100">
-    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
-      <el-button round @click="$router.push('Start')">Nuovo Gioco</el-button>
+    <el-col :span="24">
+      <h1 style="color: white" class="center-text">EXPERIENCE</h1>
     </el-col>
-    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
-      <el-button round  @click="$router.push('Game')" :disabled="getResumeGame()">Riprendi Partita</el-button>
+    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12" class="center-button">
+        <el-button class="firstButton" round @click="$router.push('Start')"
+        >Nuovo Gioco</el-button
+      >
+    </el-col>
+    <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12" class="center-button">
+      <el-button
+        class="secondButton"
+        round
+        @click="$router.push('Game')"
+        :disabled="getResumeGame()"
+        >Riprendi Partita</el-button
+      >
     </el-col>
   </el-row>
 </template>
@@ -25,7 +36,26 @@ export default {
 }
 </script>
 <style scoped>
-.el-button {
-  margin: 20px;
+.center-text {
+  margin-left: auto;
+  margin-right: auto;
+  padding: 10px;
+  text-align: center;
+}
+.center-button{
+  position: fixed;
+  margin-left: auto;
+  margin-right: auto;
+}
+.firstButton {
+  top: 20%;
+}
+.secondButton {
+  top: 40%;
+}
+.el-button{
+  height: 7%;
+  width: 50%;
+  position: fixed;
 }
 </style>
